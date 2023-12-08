3729c0e3a9670ac7a972a04f7d7ed076b5c8bbc6
lcontr1 <contrerasliliana22@gmail.com>
2023-12-08 15:21:46 -0600

    puppies featured and styled

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -6,12 +6,31 @@ import { useState } from 'react'
 function App() {
   
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => 
+    pup.id === featPupId
+  )
+  console.log(featuredPup)
+
   console.log(puppies)
   return (
       <div>
         {puppies.map((puppy) =>{
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p className='puppies' onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         })}
+
+
+        { featPupId && (
+          <div className='individualPup'>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+
       </div>
   )
 }
